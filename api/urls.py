98333831497aee385594ff66468e292dbaccb1b5
dipu0 <c.dipu0@gmail.com>
2024-02-21 05:06:09 +0600

    [wip] browser api gata getting

--- api/urls.py
+++ api/urls.py
@@ -17,5 +17,10 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
+#login
+# CMD: curl -X POST http://127.0.0.1:8000/api/token/ -d "username=dipu&password=dipu"
+#windows poweshell: Invoke-RestMethod -Method Post -Uri http://127.0.0.1:8000/api/token/ -Body "username=dipu&password=dipu"
 
-#toke login curl : curl -X POST http://127.0.0.1:8000/api/token/ -d "username=spider&password=vinayak"
+#get task
+
+#curl -i -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzA4NDcwMDU4LCJpYXQiOjE3MDg0NjgyNTgsImp0aSI6IjVjYzBmZDRkMmM0MzQxYmViZmJlOTQ3NTc2NWU5ZWFhIiwidXNlcl9pZCI6MX0.AfhSt5aKNrU_1WLhqUyg5voNzoITAHB-koH5bnTIHBs" http://127.0.0.1:8000/api/tasks/
--- todolist/settings.py
+++ todolist/settings.py
@@ -135,16 +135,69 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        # Maintain JWT Authentication for API-centric access
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
+
+        # Allow session-based authentication for website-based requests
+        'rest_framework.authentication.SessionAuthentication',
+
+        # Allow token-based authentication (useful for machine-machine or decoupled use cases)
+        'rest_framework.authentication.TokenAuthentication',
+
+        # Allow API requests to be made from the browser using the same approach as a user agent
+        'rest_framework.authentication.BasicAuthentication',
+    )
 }
 
+
 from datetime import timedelta
+from django.conf import settings
+
+# SIMPLE_JWT = {
+#     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+#     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+#     'ROTATE_REFRESH_TOKENS': False,
+#     'BLACKLIST_AFTER_ROTATION': True,
+#     'UPDATE_LAST_LOGIN': False,
+# }
+
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'ROTATE_REFRESH_TOKENS': False,
-    'BLACKLIST_AFTER_ROTATION': True,
-    'UPDATE_LAST_LOGIN': False,
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=999),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": False,
+    "UPDATE_LAST_LOGIN": False,
+
+    "ALGORITHM": "HS256",
+    "SIGNING_KEY": settings.SECRET_KEY,
+    "VERIFYING_KEY": "",
+    "AUDIENCE": None,
+    "ISSUER": None,
+    "JSON_ENCODER": None,
+    "JWK_URL": None,
+    "LEEWAY": 0,
+
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",
+    "USER_AUTHENTICATION_RULE": "rest_framework_simplejwt.authentication.default_user_authentication_rule",
+
+    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+    "TOKEN_TYPE_CLAIM": "token_type",
+    "TOKEN_USER_CLASS": "rest_framework_simplejwt.models.TokenUser",
+
+    "JTI_CLAIM": "jti",
+
+    "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
+    "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
+    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
+
+    "TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainPairSerializer",
+    "TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSerializer",
+    "TOKEN_VERIFY_SERIALIZER": "rest_framework_simplejwt.serializers.TokenVerifySerializer",
+    "TOKEN_BLACKLIST_SERIALIZER": "rest_framework_simplejwt.serializers.TokenBlacklistSerializer",
+    "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
+    "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
